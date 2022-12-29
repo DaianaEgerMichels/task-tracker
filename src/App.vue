ec8d9612b943f0cb29e88625738ac31a167d84cd
Daiana Michels <daianaem.12@gmail.com>
2022-12-28 21:55:36 -0300

    adds vue-router

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
       <SideBar @theChangedTheme="changeTheme"/>
     </div>
     <div class="column is-three-quarter content">
-      <TheForm @whenSavingTask="saveTask" />
-      <div class="lista">
-        <TaskItem v-for="(task, index) in tasks" :key="index" :task="task" />
-        <Box v-if="listIsEmpty"> You are not very productive today :( </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -16,36 +12,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SideBar from "./components/SideBar.vue";
-import TheForm from "./components/TheForm.vue";
-import TaskItem from "./components/Task.vue";
-import ITask from "./interfaces/ITask";
-import Box from "./components/Box.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     SideBar,
-    TheForm,
-    TaskItem,
-    Box,
   },
   data() {
     return {
-      tasks: [] as ITask[],
       darkModeActive: false
     };
   },
   methods: {
-    saveTask(task: ITask) {
-      this.tasks.push(task);
-    },
     changeTheme(darkModeActive: boolean) {
         this.darkModeActive = darkModeActive
-    }
-  },
-  computed: {
-    listIsEmpty () : boolean {
-      return this.tasks.length === 0
     }
   },
 });
---
+++ src/views/Tasks.vue
@@ -0,0 +1,39 @@
+<template>
+  <TheForm @whenSavingTask="saveTask" />
+  <div class="lista">
+    <TaskItem v-for="(task, index) in tasks" :key="index" :task="task" />
+    <Box v-if="listIsEmpty"> You are not very productive today :( </Box>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheForm from "../components/TheForm.vue";
+import TaskItem from "../components/Task.vue";
+import ITask from "../interfaces/ITask";
+import Box from "../components/Box.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TheForm,
+    TaskItem,
+    Box,
+  },
+  data() {
+    return {
+      tasks: [] as ITask[],
+    };
+  },
+  methods: {
+    saveTask(task: ITask) {
+      this.tasks.push(task);
+    },
+  },
+  computed: {
+    listIsEmpty(): boolean {
+      return this.tasks.length === 0;
+    },
+  },
+});
+</script>

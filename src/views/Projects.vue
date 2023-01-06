df57ba411be682cff4fe42bf87546188b00c5f2e
Daiana Michels <daianaem.12@gmail.com>
2023-01-05 22:24:17 -0300

    adds form to add a new project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,66 @@
 <template>
-    <div>
-        <h1>Projects</h1>
-    </div>
+    <section class="projects">
+        <h1 class="title">Projects</h1>
+        <form @submit.prevent="save">
+            <div class="field">
+                <label for="projectName" class="label">
+                    Project Name: 
+                </label>
+                <input type="text" class="input" v-model="projectName" id="projectName"/>
+            </div>
+           <div class="field">
+                <button class="button" type="submit">
+                    Save
+                </button>
+           </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="project in projects" :key="project.id">
+                    <td>{{project.id}}</td>
+                    <td>{{project.name}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </template>
 
 <script lang=ts>
 import { defineComponent } from "vue";
+import IProject from '../interfaces/IProject'
 
     export default defineComponent({
         // eslint-disable-next-line vue/multi-word-component-names
         name:'Projects',
+        data () {
+            return {
+                projectName: "",
+                projects: [] as IProject[]
+            };
+        },
+        methods: {
+            save () {
+                const project: IProject = {
+                    name: this.projectName,
+                    id: new Date().toISOString()
+                }
+                this.projects.push(project)
+                console.log(project);
+                this.projectName = ''
+            }
+        }
         
     })
 </script>
+
+<style scoped>
+.projects {
+    padding: 1.25rem;
+}
+</style>

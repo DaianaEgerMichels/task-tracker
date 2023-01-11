acc23b78e95f8880995e28316a6499e6b7b61526
Daiana Michels <daianaem.12@gmail.com>
2023-01-10 22:08:00 -0300

    adds new page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,67 +1,55 @@
 <template>
-    <section class="projects">
-        <h1 class="title">Projects</h1>
-        <form @submit.prevent="save">
-            <div class="field">
-                <label for="projectName" class="label">
-                    Project Name: 
-                </label>
-                <input type="text" class="input" v-model="projectName" id="projectName"/>
-            </div>
-           <div class="field">
-                <button class="button" type="submit">
-                    Save
-                </button>
-           </div>
-        </form>
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="project in projects" :key="project.id">
-                    <td>{{project.id}}</td>
-                    <td>{{project.name}}</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
+  <section class="projects">
+    <h1 class="title">Projects</h1>
+    <router-link to="/projects/new" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>New Project</span>
+    </router-link>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+          <td>
+            <router-link :to="`/projects/${project.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
-<script lang=ts>
+<script lang="ts">
 import { computed, defineComponent } from "vue";
-import { useStore } from "@/store";2
+import { useStore } from "@/store";
 
-    export default defineComponent({
-        // eslint-disable-next-line vue/multi-word-component-names
-        name:'Projects',
-        data () {
-            return {
-                projectName: "",
-            };
-        },
-        methods: {
-            save () {
-               this.store.commit('ADD_PROJECT', this.projectName)
-               this.projectName = "";
-            }
-        },
-        setup (){
-            const store = useStore()
-            return {
-                store,
-                projects: computed(() => store.state.projects)
-            }
-        }
-        
-    })
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Projects",
+  setup() {
+    const store = useStore();
+    return {
+      projects: computed(() => store.state.projects),
+    };
+  },
+});
 </script>
 
 <style scoped>
 .projects {
-    padding: 1.25rem;
+  padding: 1.25rem;
 }
 </style>

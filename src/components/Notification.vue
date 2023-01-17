0371f20cde945f0eb74ceea7d1dd8e643210f9cd
Daiana Michels <daianaem.12@gmail.com>
2023-01-16 21:38:55 -0300

    creates notifications

---
+++ src/components/Notification.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="notification">
+        <article class="message is-success">
+            <div class="message-header">
+                Success!
+            </div>
+            <div class="message-body">
+                Save with success :)
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">
+                Warning!
+            </div>
+            <div class="message-body">
+                Ooops :/
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                Error!
+            </div>
+            <div class="message-body">
+                Ooh no :(
+            </div>
+        </article>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        // eslint-disable-next-line vue/multi-word-component-names
+        name: 'Notification',
+    })
+</script>
+
+<style scoped>
+.notification {
+    position: absolute;
+    right: 0;
+    width: 300px;
+    padding: 8px;
+    z-index: 105;
+}
+</style>
